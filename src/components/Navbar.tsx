3b1bd3e0aff8fb87d41bef9394bad2aaa055c4ef
uomoazione <manuelcampi20@gmail.com>
2023-12-22 16:25:23 +0100

    Added dynamic component  Navbar
    Also the hamburger button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,19 @@
 // Author: Sbenduel
 
 export const Navbar = () => {
+  //Lista stub di link per la navigazione, current indica la pagina attuale
+  const navigation = [
+    { name: "Home", href: "#", current: true },
+    { name: "PlaceHolder", href: "#", current: false },
+    { name: "PlaceHolder", href: "#", current: false },
+    { name: "PlaceHolder", href: "#", current: false },
+  ];
+
+  //Funzione per capire se una classe merita il css true o false durante il rendering
+  function classNames(...classes: string[] | boolean[]) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   return (
     <nav className=" bg-[#112d4e] px-4 sm:px-8 lg:px-20 relative flex items-center justify-between h-16">
       <div className="flex items-center">
@@ -14,19 +27,39 @@ export const Navbar = () => {
           src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
           alt="Workflow logo"
         />
-        <div className="text-white flex space-x-4 ml-8">
-          <a href="#">Home</a>
-          <a>PlaceHolder</a>
-          <a>Placeholder</a>
-          <a>Placeholder</a>
+        <div className=" lg:flex hidden text-white space-x-4 ml-8">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className={classNames(
+                item.current
+                  ? "bg-[#4F709C] text-white"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                "block rounded-md px-3 py-2 text-base font-medium"
+              )}
+              aria-current={item.current ? "page" : undefined}
+            >
+              {item.name}
+            </a>
+          ))}
         </div>
       </div>
       <button
         type="button"
-        className="bg-[#F4F4F4] hover:bg-white text-[#112d4e] font-bold py-2 px-4 rounded"
+        className="bg-[#F4F4F4] hover:bg-[#FCD94F] text-[#112d4e] font-bold py-2 px-4 rounded"
       >
         Login
       </button>
+      <div className="lg:hidden block">
+        <button>
+          <img
+            className="w-auto h-fit"
+            src={"public/imgs/hamburgerMenu.svg"}
+            alt="Hamburger Menu"
+          />{" "}
+        </button>
+      </div>
     </nav>
   );
 };
