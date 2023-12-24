9c86103dfc3fa0688585ad69cacef7d98bbc674e
kyl00h <raedaa.bou@gmail.com>
2023-12-24 09:49:48 +0100

    fixed placeholder and unused import, searchbox put inside Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import { Searchbox } from "./Searchbox";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home</div>
+      <Searchbox />
+    </>
+  );
+};
