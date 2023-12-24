7599fc22c09c7143ea6fd0845f8bf8924780678b
kyl00h <raedaa.bou@gmail.com>
2023-12-24 11:20:39 +0100

    created an initial videocamera card component layout, still have to put props etc...

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,24 @@
 import { Searchbox } from "./Searchbox";
+import { VideocameraCard } from "./VideocameraCard";
 
 export const Home = () => {
+  const cards = [<VideocameraCard />];
   return (
     <>
       <div>Home</div>
       <Searchbox />
+      <div className="flex justify-center">
+        {/* Se ci sta solo una card, la centra */}
+        <div
+          className={`${
+            cards.length === 1
+              ? "flex justify-center"
+              : "grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-20"
+          }`}
+        >
+          {cards}
+        </div>
+      </div>
     </>
   );
 };
