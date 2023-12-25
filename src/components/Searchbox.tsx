0c61e2762448d89b07c1d0f73199bd5daf96218c
Rida Abou <133781192+kyl00h@users.noreply.github.com>
2023-12-25 17:46:58 +0100

    fixed some classes and changed font

--- src/components/Searchbox.tsx
+++ src/components/Searchbox.tsx
@@ -6,34 +6,32 @@ type SearchBoxProps = { datepickerIcon?: boolean };
 export const Searchbox = (props: SearchBoxProps) => {
   return (
     <>
-      <div className="max-w-md mx-auto overflow-hidden md:max-w-xl">
-        <div className="md:flex">
-          <div className="w-full p-3">
-            <form>
-              <div className="relative shadow-md">
-                <button>
-                  <i className="absolute fa fa-search text-black top-5 left-4"></i>
-                </button>
-                <input
-                  type="text"
-                  placeholder={
-                    props.datepickerIcon
-                      ? "Che filmato stai cercando?"
-                      : "Che telecamera stai cercando?"
-                  }
-                  className=" placeholder-gray-700 bg-yellow-400 h-14 w-full px-12 rounded-sm focus:outline-none hover:cursor-pointer"
-                  name="search"
-                />
-                {props.datepickerIcon && (
-                  <span className="absolute top-4 right-5 border-slate-600 border-l pl-4">
-                    <i className="fa-regular fa-calendar text-black hover:text-gray-100 hover:cursor-pointer"></i>
-                  </span>
-                )}
-              </div>
-            </form>
+      <form className="my-8 p-5">
+        <div className="relative max-w-3xl  mx-auto  ">
+          <div className="absolute inset-y-0 start-0  flex items-center ps-3 pointer-events-none">
+            <button>
+              <i className="absolute fa fa-search text-black top-5 left-4"></i>
+            </button>
           </div>
+          <input
+            type="search"
+            id="default-search"
+            className="h-14 rounded-sm shadow-xl placeholder-gray-700 block w-full p-4 ps-10  Inter bg-yellow-400  focus:outline-none text-sm   hover:cursor-pointer "
+            placeholder={
+              props.datepickerIcon
+                ? "Che filmato stai cercando?"
+                : "Che telecamera stai cercando?"
+            }
+            required
+            name="search"
+          />
+          {props.datepickerIcon && (
+            <span className="absolute top-4 right-5 bg-yellow-400 border-slate-600 border-l pl-4">
+              <i className="fa-regular fa-calendar text-black hover:text-gray-100 hover:cursor-pointer"></i>
+            </span>
+          )}
         </div>
-      </div>
+      </form>
     </>
   );
 };
