6bdae1d5f815c19123f5d11f235b8081c073b7ad
uomoazione <manuelcampi20@gmail.com>
2024-01-09 09:10:59 +0100

    Updated nested path and Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { TestNavbar } from "./components/TestNavbar";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
 
+const Layout = () => (
+  <>
+    <TestNavbar />
+    <Outlet />
+  </>
+);
+
 export const App = () => {
   return (
-    <>
-      <TestNavbar />
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path=":id" element="DetailCamera" />
-          <Route path="favorites" element="FavoriteCamera" />
-        </Route>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path=":id" element="DetailCamera" />
+        <Route path="favorites" element="FavoriteCamera" />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
   );
 };
 
